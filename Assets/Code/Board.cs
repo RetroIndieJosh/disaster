45e7421a86672fef6edbd35dad91a353dd62cb21
Joshua McLean <mrjoshuamclean@gmail.com>
2020-03-14 23:39:57 -0400

    Create board

--- Assets/Code/Board.cs
+++ Assets/Code/Board.cs
@@ -1,19 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Board : MonoBehaviour
 {
     [SerializeField] private int m_size = 6;
-    [SerializeField]
+    [SerializeField] private BoardTile m_tilePrefab = null;
 
-    void Start()
-    {
-        
+    private BoardTile[,] m_tileMap = null;
+
+    private void Start() {
+        m_tileMap = new BoardTile[m_size, m_size];
+        CreateTileButtons();
+        LinkTileButtons();
     }
 
-    void Update()
-    {
-        
+    private void CreateTileButtons() {
+        var pos = Vector2Int.zero;
+        var tileRect = m_tilePrefab.GetComponent<RectTransform>().rect;
+        var tileWidth = Mathf.FloorToInt(tileRect.width);
+        var tileHeight = Mathf.FloorToInt(tileRect.height);
+        for (var y = 0; y < m_size; ++y) {
+            for (var x = 0; x < m_size; ++x) {
+                m_tileMap[x, y] = Instantiate(m_tilePrefab, transform);
+                var rectTransform = m_tileMap[x, y].GetComponent<RectTransform>();
+                rectTransform.anchoredPosition = pos;
+                pos.x += tileHeight;
+            }
+            pos.x = 0;
+            pos.y -= tileWidth;
+        }
+    }
+
+    private void LinkTileButtons() {
+        for (var y = 0; y < m_size; ++y) {
+            for (var x = 0; x < m_size; ++x) {
+                var button = m_tileMap[x, y].GetComponent<Button>();
+            }
+        }
     }
 }
--- Assets/Code/BoardTile.cs
+++ Assets/Code/BoardTile.cs
@@ -2,17 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum BoardTileState
+{
+    Clear = 0,
+    Black = 1,
+    White = 2,
+    Fire = 3,
+    Flood = 4,
+    Plague = 5
+}
+
+public enum Direction
+{
+    None = 0,
+    East = 1,
+    North = 2,
+    Northeast = 3,
+    Northwest = 4,
+    South = 5,
+    Southeast = 6,
+    Southwest = 7,
+    West = 8
+}
+
+public enum PlayerColor {
+    None = 0,
+    Black = 1,
+    White = 2
+}
+
 public class BoardTile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private Direction m_facing = Direction.None;
+    private PlayerColor m_controller = PlayerColor.None;
 }
